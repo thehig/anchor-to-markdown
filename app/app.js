c9385847221f5c0517a48039a30612e25610bd3e
David Higgins <thehig@gmail.com>
2016-12-04 18:00:03 +0000

    Add console out and more configuration options (0.0.3)
    * Extract input and output files to config.json
    * Convert include and exclude from singular regexes into arrays of regexes
    * Add debug output logging
    * Refactor promise returns to be single line
    * Refactor 'bookmark' to 'anchor'
    * Add more useful output inbetween named promises

--- app/app.js
+++ app/app.js
@@ -1,29 +1,48 @@
 const   jsdom = require("jsdom"),
         fs = require('fs'),
         path = require('path'),
-        config = require('../data/config.json');
+        config = require('../data/config.json'),
+        _ = require('lodash');
 
+const allDebugEnabled = false;
+const debug = {
+    logFn: allDebugEnabled || false // Output the name of the function when called
+    , logRegexMatching: allDebugEnabled || false // Output the regex matching process
+    , logFilter: allDebugEnabled || false // Output include/exclude/unknown
+    , logMarkdown: allDebugEnabled || false // Output the markdown to the console
+}
 
-let readFileAsync = url =>
-    new Promise((c, e)=>{
-        if(typeof url === undefined) e(new Error("No url to load"));
-        fs.readFile(path.resolve(__dirname, url), "utf-8", (err, data)=>{
+let readFileAsync = () => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] readFileAsync()`);
+        if(typeof config === undefined) return e(new Error("config not loaded in readFileAsync"));
+        if(typeof config.input === undefined) return e(new Error("config.input not loaded in readFileAsync"));
+        fs.readFile(path.resolve(__dirname, config.input), "utf-8", (err, data)=>{
             err ? e(err) : c(data);
         });
     });
 
-let loadJsdomAsync = html =>
-    new Promise((c, e)=>{
-        if(typeof html === undefined) e(new Error("No HTML for JSDOM"));
+let writeFileAsync = inputString => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] writeFileAsync(${config.output})`);
+        if(typeof inputString === undefined) return e(new Error("No inputString in writeFileAsync"));
+        if(typeof config === undefined) return e(new Error("config not loaded in writeFileAsync"));
+        if(typeof config.output === undefined) return e(new Error("config.output not loaded in writeFileAsync"));
+        fs.writeFile(path.resolve(__dirname, config.output), inputString, {flags: 'w'}, (err)=>{
+            err ? e(err) : c();
+        });
+    });
+
+let loadJsdomAsync = html => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] loadJsdomAsync()`);
+        if(typeof html === undefined) return e(new Error("No HTML in loadJsdomAsync"));
         jsdom.env(html, ["http://code.jquery.com/jquery.js"], (err, window)=>{
             err ? e(err) : c(window);
         });
     });
 
-let scrapeBookmarksAsync = window =>
-    new Promise((c, e)=>{
-        if(typeof window === undefined) e(new Error("No window from JSDOM"));
-        if(typeof window.$ === undefined) e(new Error("No jquery in window from JSDOM"));
+let scrapeAnchorsAsync = window => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] scrapeAnchorsAsync()`);
+        if(typeof window === undefined) return e(new Error("No window in scrapeAnchorsAsync"));
+        if(typeof window.$ === undefined) return e(new Error("No jquery in scrapeAnchorsAsync"));
         let $ = window.$;
         let titles = [];
         try{
@@ -42,12 +61,16 @@ let scrapeBookmarksAsync = window =>
         c(titles);
     });
 
-let filterBookmarksAsync = bookmarks =>
-    new Promise((c, e)=>{
-        if(typeof bookmarks === undefined) e(new Error("No bookmarks from scrapeBookmarksAsync"));
-        if(typeof config === undefined) e(new Error("No config loaded"));
-        let includeRegex = new RegExp(config.include, 'i'); // Ignore case
-        let excludeRegex = new RegExp(config.exclude, 'i'); // Ignore case
+let filterAnchorsAsync = anchors => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] filterAnchorsAsync(${anchors.length} anchors)`);
+        if(typeof anchors === undefined) return e(new Error("No anchors in filterAnchorsAsync"));
+        if(typeof config === undefined) return e(new Error("No config loaded"));
+        if(typeof config.include === undefined) return e(new Error("No config.include loaded"));
+        if(typeof config.exclude === undefined) return e(new Error("No config.exclude loaded"));
+
+        let createRegex = regexString => new RegExp(regexString, 'i');
+        let includeRegexs = config.include.map(createRegex);
+        let excludeRegexs = config.exclude.map(createRegex);
         let filtered = {
             include: [],
             exclude: [],
@@ -55,13 +78,29 @@ let filterBookmarksAsync = bookmarks =>
         }
 
         try{
-            bookmarks.forEach((bookmark)=>{
-                if(excludeRegex.test(bookmark.url) || excludeRegex.test(bookmark.text)) 
-                    filtered.exclude.push(bookmark);
-                else if(includeRegex.test(bookmark.url) || includeRegex.test(bookmark.text)) 
-                    filtered.include.push(bookmark);
-                else
-                    filtered.unknown.push(bookmark);
+            anchors.forEach((anchor)=>{
+                let matchesRegex = (regex) => {
+                    let matchesUrl = regex.test(anchor.url);
+                    let matchesText = regex.test(anchor.text);
+                    if(typeof debug !== undefined && debug.logRegexMatching == true){
+                        if(matchesUrl) console.log(`[*] Regex ${regex} matches url "${anchor.url}"`);
+                        if(matchesText) console.log(`[*] Regex ${regex} matches text "${anchor.text}"`);
+                    }
+                    return matchesUrl || matchesText;
+                }
+
+                if(_.some(excludeRegexs, matchesRegex)){
+                    if(typeof debug !== undefined && debug.logFilter == true) console.log(`[-] Filter Excluding:\t "${anchor.text}"`);
+                    filtered.exclude.push(anchor);
+                }
+                else if(_.some(includeRegexs, matchesRegex)){
+                    if(typeof debug !== undefined && debug.logFilter == true) console.log(`[+] Filter Including:\t "${anchor.text}"`);
+                    filtered.include.push(anchor);
+                }
+                else{
+                    if(typeof debug !== undefined && debug.logFilter == true) console.log(`[?] Filter Unknown:\t "${anchor.text}"`);
+                    filtered.unknown.push(anchor);
+                }
             });
         } catch(err){ 
             e(err); 
@@ -71,48 +110,53 @@ let filterBookmarksAsync = bookmarks =>
 
 let reverseListAync = list => Promise.resolve(list.reverse());
 
-let printBookmark = (maxText, maxUrl, bookmark) => 
-    `${bookmark.index} :\t${bookmark.text.substring(0, maxText)} --\t${bookmark.url.substring(0, maxUrl)}`;
-
-let printShort = printBookmark.bind(null, 50, 30);
-let printLong = printBookmark.bind(null, 1000, 1000);
-let printTextOnly = printBookmark.bind(null, 1000, 0);
-let printUrlOnly = printBookmark.bind(null, 0, 1000);
-
-let generateMarkdownFromBookmark = (previous, bookmark) => 
-    "" + previous + `* *(${bookmark.hostname})* [${bookmark.text}](${bookmark.url})\n`;
-
-let createMarkdownAsync = filteredBookmarks =>
-    new Promise((c, e)=>{
-        if(typeof filteredBookmarks === undefined) e(new Error("No bookmarks from filterBookmarksAsync"));
+let createMarkdownAsync = filteredAnchors => new Promise((c, e)=>{
+        if(typeof debug !== undefined && debug.logFn == true) console.log(`[*] createMarkdownAsync(${filteredAnchors.include.length} anchors)`);
+        if(typeof filteredAnchors === undefined) return e(new Error("No anchors in createMarkdownAsync"));
 
         let outputMarkdown = "";
         try{
-            outputMarkdown = filteredBookmarks.include.reduce(generateMarkdownFromBookmark, "# Automated bookmark filtration\n\n");
+            outputMarkdown = filteredAnchors.include.reduce((previous, anchor) => {
+                return "" + previous + `* *(${anchor.hostname})* [${anchor.text}](${anchor.url})\n`;
+            }, "# Automated anchor filtration\n\n");
         } catch(err){
             e(err);
         }
 
+        if(typeof debug !== undefined && debug.logMarkdown == true) console.log(`[*] ===== Output Markdown =====\n${outputMarkdown}\n[*] ==== /Output Markdown/ ====`);
         c(outputMarkdown);
     });
 
-let outputMarkdownAsync = markdownText =>
-    new Promise((c, e)=>{
-        if(typeof markdownText === undefined) e(new Error("No markdownText from createMarkdownAsync"));
-        fs.writeFile(path.resolve(__dirname, '../data/bookmarks.md'), markdownText, {flags: 'w'}, (err)=>{
-            err ? e(err) : c();
-        });
-    });
+
+
+// let anchorToString = (maxText, maxUrl, anchor) =>  
+//     `${anchor.index} :\t${anchor.text.substring(0, maxText)} --\t${anchor.url.substring(0, maxUrl)}`;
+
+// let printShort = anchorToString.bind(null, 50, 30);
+// let printLong = anchorToString.bind(null, 1000, 1000);
+// let printTextOnly = anchorToString.bind(null, 1000, 0);
+// let printUrlOnly = anchorToString.bind(null, 0, 1000);
+
 
 readFileAsync('../data/bookmarks.html')
     .then(loadJsdomAsync)
-    .then(scrapeBookmarksAsync)
+    .then(scrapeAnchorsAsync)
+    .then ( anchorsList => {
+        console.log(`[+] Got ${anchorsList.length} anchors`);
+        return anchorsList;
+    })
     .then(reverseListAync)
-    .then(filterBookmarksAsync)
+    .then(filterAnchorsAsync)
+    .then( filteredAnchors => {
+        console.log(`[+] ${filteredAnchors.exclude.length} exclude items`);
+        console.log(`[+] ${filteredAnchors.include.length} include items`);
+        console.log(`[+] ${filteredAnchors.unknown.length} unknown items`);
+        return filteredAnchors;
+    })
     .then(createMarkdownAsync)
-    .then(outputMarkdownAsync)
+    .then(writeFileAsync)
     .then(()=>{
-        console.log("[+] Success");
+        console.log("[+] Complete");
     })
     .then(null, (err)=>{
         console.log("[-] Error");
